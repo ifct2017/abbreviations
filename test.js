39f698bc4496623ff57e5544b9bc53fea8520892
Subhajit Sahu <wolfram77@gmail.com>
2021-03-07 00:06:17 +0530

    :ringed_planet: improve build, test

--- test.js
+++ test.js
@@ -6,12 +6,15 @@ const abbreviations = require('./')
 
 function testAll() {
   var a = abbreviations('GLV')
-  assert.deepStrictEqual(a, {abbr: 'GLV', full: 'Green Leafy Vegetables'});
-  var a = abbreviations('g l v');
-  assert.deepStrictEqual(a, {abbr: 'GLV', full: 'Green Leafy Vegetables'});
+  var b = abbreviations('g l v');
+  var c = {abbr: 'GLV', full: 'Green Leafy Vegetables'};
+  assert.deepStrictEqual(a, c);
+  assert.deepStrictEqual(b, c);
+
   var a = abbreviations('what is D.R.I.');
-  assert.deepStrictEqual(a, {abbr: 'DRI', full: 'Dietary reference intake'});
-  var a = abbreviations('d. r. i. stands for?');
-  assert.deepStrictEqual(a, {abbr: 'DRI', full: 'Dietary reference intake'});
+  var b = abbreviations('d. r. i. stands for?');
+  var c = {abbr: 'DRI', full: 'Dietary reference intake'};
+  assert.deepStrictEqual(a, c);
+  assert.deepStrictEqual(b, c);
 }
 testAll();
